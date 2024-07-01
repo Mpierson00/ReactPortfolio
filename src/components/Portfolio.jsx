1c13e59f832bb3bf5711399c537b009382d0a47c
Michael <mpierson00@gmail.com>
2024-06-30 23:31:55 -0400

    added portfolios

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Project from './Project';
+
+const Portfolio = () => (
+  <section>
+    <h2>Portfolio</h2>
+    <Project title="Project 1" />
+    <Project title="Project 2" />
+    <Project title="Project 3" />
+    <Project title="Project 4" />
+    <Project title="Project 5" />
+    <Project title="Project 6" />
+  </section>
+);
+
+export default Portfolio;
