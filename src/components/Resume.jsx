642399c6bc6e0eea77fa0016d23920d00693a86c
Michael <mpierson00@gmail.com>
2024-06-30 23:34:09 -0400

    added resume

---
+++ src/components/Resume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Resume = () => (
+  <section>
+    <h2>Resume</h2>
+    <p>Link to resume or detailed resume here.</p>
+  </section>
+);
+
+export default Resume;
