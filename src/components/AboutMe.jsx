9fc10b9f3774844990b8782da54b671a3d75ef4d
Michael <mpierson00@gmail.com>
2024-07-01 01:59:57 -0400

    added about me styling

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,17 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const AboutMe = () => (
   <section>
     <h2>About Me</h2>
-    <img src="/images/myphoto.jpg" alt="My Avatar" />
-    <p>
-      I am a new full stack developer. I am dedicated and results driven. I
-      thrive in dynamic environments where I can leverage my creativity and
-      strategic thinking, and collaborative nature to solve complex challenges.
-      I am passionate about Full stack development and committed to continuous
-      learning and professional development. I am confident that I can adapt to
-      any team or organization..
-    </p>
+    <div className="about-me-content">
+      <img src="/images/myphoto.jpg" alt="My Avatar" />
+      <p>
+        I am a new full stack developer. I am dedicated and results-driven. I thrive in dynamic environments where I can leverage my creativity, strategic thinking, and collaborative nature to solve complex challenges. I am passionate about full stack development and committed to continuous learning and professional development. I am confident that I can adapt to any team or organization.
+      </p>
+    </div>
   </section>
 );
 
