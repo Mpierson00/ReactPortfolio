b0074574e29e7d4b2ba17f2b159af8fd3fb9753c
Michael <mpierson00@gmail.com>
2024-07-04 16:39:47 -0400

    updated porfolio to add repo link

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,18 +11,21 @@ const Portfolio = () => {
       description: "A web application to find pets and help you name them.",
       gif: pawfinderGif,
       link: "https://mpierson00.github.io/Pawfinder/",
+      repo: "https://github.com/Mpierson00/Pawfinder",
     },
     // {
     //   title: 'Workday Scheduler',
     //   description: 'A simple calendar application to schedule your workday.',
     //   gif: workdaySchedulerGif,
     //   link: 'https://mpierson00.github.io/Workday-Scheduler/',
+    //   repo: 'https://github.com/Mpierson00/Workday-Scheduler'
     // },
     // {
     //   title: 'Weather Application',
     //   description: 'A weather application to check the current weather.',
     //   gif: weatherApplicationGif,
     //   link: 'https://mpierson00.github.io/WeatherApplication/',
+    //  repo: 'https://github.com/Mpierson00/WeatherApplication'
     // },
   ];
 
@@ -37,6 +40,7 @@ const Portfolio = () => {
             description={project.description}
             gif={project.gif}
             link={project.link}
+            repo={project.repo}
           />
         ))}
       </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Project = ({ title, description, gif, link }) => {
+const Project = ({ title, description, gif, link, repo }) => {
   return (
-    <div className='="project'>
+    <div className="project">
       <h3>{title}</h3>
       <a href={link} target="_blank" rel="noopener noreferrer">
         <img src={gif} alt={`${title} GIF`} />
       </a>
       <p>{description}</p>
+      <a href={repo} target="_blank" rel="noopener noreferrer">GitHub Repository</a>
     </div>
   );
 };
