47ca17f17bd386f56c994256aec671d0a41056e8
Michael <mpierson00@gmail.com>
2024-07-04 16:11:04 -0400

    added porfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
-import Project from './Project';
+import React from "react";
+import Project from "./Project";
+import pawfinderGif from "../assets/gifs/pawfinder.gif";
+// import workdaySchedulerGif from '../assets/gifs/workday-scheduler.gif';
+// import weatherApplicationGif from '../assets/gifs/weather-application.gif';
 
-const Portfolio = () => (
-  <section>
-    <h2>Portfolio</h2>
-    <Project title="Project 1" />
-    <Project title="Project 2" />
-    <Project title="Project 3" />
-    <Project title="Project 4" />
-    <Project title="Project 5" />
-    <Project title="Project 6" />
-  </section>
-);
+const Portfolio = () => {
+  const projects = [
+    {
+      title: "Pawfinder",
+      description: "A web application to find pets and help you name them.",
+      gif: pawfinderGif,
+      link: "https://mpierson00.github.io/Pawfinder/",
+    },
+    // {
+    //   title: 'Workday Scheduler',
+    //   description: 'A simple calendar application to schedule your workday.',
+    //   gif: workdaySchedulerGif,
+    //   link: 'https://mpierson00.github.io/Workday-Scheduler/',
+    // },
+    // {
+    //   title: 'Weather Application',
+    //   description: 'A weather application to check the current weather.',
+    //   gif: weatherApplicationGif,
+    //   link: 'https://mpierson00.github.io/WeatherApplication/',
+    // },
+  ];
+
+  return (
+    <section id="portfolio">
+      <h2>Portfolio</h2>
+      <div className="portfolio">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            description={project.description}
+            gif={project.gif}
+            link={project.link}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Portfolio;
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Project = ({ title }) => (
-  <div>
-    <h3>{title}</h3>
-    <img src="/images/project.jpg" alt={title} />
-  </div>
-);
+const Project = ({ title, description, gif, link }) => {
+  return (
+    <div className='="project'>
+      <h3>{title}</h3>
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <img src={gif} alt={`${title} GIF`} />
+      </a>
+      <p>{description}</p>
+    </div>
+  );
+};
 
 export default Project;
